ad557ed3af8b9e384b8458d0125c65d281823ae4
Daryl Stark <daryl@dstark.nl>
2023-04-26 22:48:00 +0200

    Added a function to set the chepeast timeblocks in HomeAssistant

--- src/energy_prices/metrics.py
+++ src/energy_prices/metrics.py
@@ -1,5 +1,8 @@
 """ Module with functions for the metrics """
 
+import time
+
+from flask import Config
 from ep_database.prices import get_power_prices
 from event_bus import Event
 from energy_prices.aggregators import get_cheapest_successive_hours
@@ -9,9 +12,14 @@ from zoneinfo import ZoneInfo
 
 from config_loader import ConfigLoader
 
+import requests
+import logging
+
 
 def get_cheapest_time_blocks(event: Event) -> None:
     """ Method that gets specific blocks of 'chepeast times' """
+
+    logger = logging.getLogger('get_cheapest_time_blocks')
 
     # Get a local timezone object
     local_timezone = ZoneInfo(ConfigLoader.config['timezone'])
@@ -34,7 +42,37 @@ def get_cheapest_time_blocks(event: Event) -> None:
     prices_tomorrow = get_power_prices(
         tomorrow, tomorrow + datetime.timedelta(hours=24))
 
-    pass
+    if prices_today is None or prices_tomorrow is None:
+        return
 
     # Get the blocks
-    # TODO: Implement
+    chepeast_blocks = {
+        'today_1': get_cheapest_successive_hours(prices_today, 1),
+        'today_2': get_cheapest_successive_hours(prices_today, 2),
+        'today_3': get_cheapest_successive_hours(prices_today, 3),
+        'today_4': get_cheapest_successive_hours(prices_today, 4),
+        'tomorrow_1': get_cheapest_successive_hours(prices_tomorrow, 1),
+        'tomorrow_2': get_cheapest_successive_hours(prices_tomorrow, 2),
+        'tomorrow_3': get_cheapest_successive_hours(prices_tomorrow, 3),
+        'tomorrow_4': get_cheapest_successive_hours(prices_tomorrow, 4)
+    }
+
+    # Send the blocks to HomeAssistant
+    for key, value in chepeast_blocks.items():
+        entity_update = requests.post(
+            url=f'{ConfigLoader.config["homeassistant"]["url"]}/api/states/input_datetime.energy_cheapest_{key}_time',
+            headers={
+                'Authorization': f'Bearer {ConfigLoader.config["homeassistant"]["token"]}',
+                'Content-Type': 'application/json'
+            },
+            json={
+                'state': value.start.time.strftime('%H:%M:%S')
+            },
+            verify=False,
+            timeout=30
+        )
+        if entity_update.status_code != 200:
+            logger.error(
+                f'Error while updating Home Assistant: {entity_update.status_code}')
+
+    pass
